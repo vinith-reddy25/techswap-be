a8a2f46b362d3c7e53c367d88b15ec3062b948bc
Rancha124 <2016eeb1089@iitrpr.ac.in>
2021-04-16 19:06:08 +0530

    user register api is created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var router = express.Router();
-
+var User = require('../models/user')
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  const {mobile} = req.body
+  User.find({mobile}).exec().then(result => {
+    console.log("result after success storing",result);
+    res.status(200).send(result)
+  }).catch(error => {
+    console.log("error in storing the info",error)
+    res.status(404).send(error)
+  })
+});
+router.post("/register", function (req, res, next) {
+  const {name,email,mobile,city} = req.body
+  const user = new User();
+  user.name = name;
+  user.email = email;
+  user.mobile = mobile;
+  user.city = city;
+  console.log("user",user)
+  user.save().then(result => {
+    console.log("result after success storing",result);
+    res.status(200).send(result)
+  }).catch(error => {
+    console.log("error in storing the info",error)
+    res.status(404).send(error)
+  })
 });
 
 module.exports = router;
